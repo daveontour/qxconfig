07d1cf20a6857b8eeea971d9f5b66267aaafb1f2
daveontour <dave_on_tour@yahoo.com>
2018-10-30 14:07:16 +0400

    Angula7 Migration Initial Working Complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Globals } from './globals';
+import { ItemConfig } from './interfaces/interfaces';
+import { SimpleComponent } from './components/simple/simple.component';
+import { SequenceComponent } from './components/sequence/sequence.component';
+import { Component, ViewChild, ViewContainerRef, ComponentFactoryResolver, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpErrorResponse } from '@angular/common/http';
+import * as $ from "jquery";
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'xsdforms';
+export class AppComponent implements OnInit{
+  @ViewChild("container", { read: ViewContainerRef }) container;
+  componentRef: any;
+  elements: any[] = [];
+  title = "AIDX Flight Messages";
+  elementID: number = 100;
+  idx: number = 4;
+  
+
+  constructor(
+    private resolver: ComponentFactoryResolver, 
+    private http: HttpClient, 
+     public global: Globals
+  ) { 
+
+  }
+
+  ngOnInit(): void {
+    this.createElement();
+  }
+
+
+  createElement() {
+
+    // Need to cater for Choice as a root element
+
+    this.http.get<ItemConfig>('http://localhost:8080/XSD_Forms/json?type=aidx').subscribe(data => {
+      console.log(data);
+      data.elementPath = data.name;
+      if (data.type == "simple") {
+        const factory = this.resolver.resolveComponentFactory(SimpleComponent);
+        this.componentRef = this.container.createComponent(factory);
+        this.componentRef.instance.setParentID("/");
+        this.componentRef.instance.isRoot = true;
+        this.componentRef.instance.setConfig(data);
+        this.global.root = this.componentRef.instance;
+      } else {
+        const factory = this.resolver.resolveComponentFactory(SequenceComponent);
+        this.componentRef = this.container.createComponent(factory);
+        this.componentRef.instance.setParentID("/");
+        this.componentRef.instance.isRoot = true;
+        this.componentRef.instance.setConfig(data);
+        this.global.root = this.componentRef.instance;
+      }
+
+      this.global.getString();
+    },
+      (err: HttpErrorResponse) => {
+        if (err.error instanceof Error) {
+          console.log('An error occurred:', err.error.message);
+        } else {
+          console.log(`Backend returned code ${err.status}, body was: ${err.error}`);
+        }
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,83 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BaseNullComponent } from './controls/basenull/basenull.component';
+import { XSDecimalComponent } from './controls/xsdecimal/xsdecimal.component';
+import { XSLanguageComponent } from './controls/xslanguage/xslanguage.component';
+import { XSIntegerComponent } from './controls/xsinteger/xsinteger.component';
+import { BaseComponent } from './controls/base/base.component';
+import { XSBooleanComponent } from './controls/xsboolean/xsboolean.component';
+import { MinMaxLengthPatternComponent } from './controls/minmaxlengthpattern/minmaxlengthpattern.component';
+import { EnumListComponent } from './controls/enumlist/enumlist.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'; 
+import { FormsModule, FormBuilder, FormGroup }   from '@angular/forms';
+import { Globals } from './globals';
+import { AceEditorModule } from 'ng2-ace-editor';
+import { SequenceComponent } from './components/sequence/sequence.component';
+import { SimpleComponent } from './components/simple/simple.component';
+import { ChoiceComponent } from './components/choice/choice.component';
+import { PatternComponent } from './controls/pattern/pattern.component';
+import { AttributeComponent } from './components/attribute/attribute.component';
+import { MinMaxLengthComponent } from './controls/minmaxlength/minmaxlength.component';
+import { MinMaxInclusiveComponent } from './controls/minmaxinclusive/minmaxinclusive.component';
+import { XSStringComponent } from './controls/xsstring/xsstring.component';
+import { XSDateComponent } from './controls/xsdate/xsdate.component';
 import { jqxSplitterComponent } from 'jqwidgets-scripts/jqwidgets-ts/angular_jqxsplitter';
+
 
 @NgModule({
   declarations: [
     AppComponent,
+    SequenceComponent,
+    SimpleComponent,
+    ChoiceComponent,
+    PatternComponent,
+    MinMaxLengthComponent,
+    MinMaxLengthPatternComponent,
+    MinMaxInclusiveComponent,
+    XSBooleanComponent,
+    XSStringComponent,
+    XSIntegerComponent,
+    XSLanguageComponent,
+    XSDecimalComponent,
+    EnumListComponent,
+    BaseComponent,
+    BaseNullComponent,
+    XSDateComponent,
+    AttributeComponent,
     jqxSplitterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    AceEditorModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    Globals
+  ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    SequenceComponent, 
+    SimpleComponent, 
+    ChoiceComponent, 
+    PatternComponent,
+    MinMaxLengthComponent,
+    MinMaxLengthPatternComponent,
+    MinMaxInclusiveComponent,
+    EnumListComponent,
+    XSBooleanComponent,
+    XSStringComponent,
+    BaseComponent,
+    BaseNullComponent,
+    XSIntegerComponent,
+    XSLanguageComponent,
+    XSDecimalComponent,
+    XSDateComponent,
+    AttributeComponent
+  ]
 })
 export class AppModule { }
